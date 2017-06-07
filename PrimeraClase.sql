b359561258d0bbf3bd41cce1d40d6a3f43eaaa57
jc <>
2017-06-07 15:54:38 -0500

    fdf

--- PrimeraClase.sql
+++ PrimeraClase.sql
@@ -1,140 +1,319 @@
-CREATE TABLE ALMACEN (NUMERO_ALMACEN INTEGER, UBICACION_ALMACEN VARCHAR2(40),
-                     CONSTRAINT PK_NA PRIMARY KEY(NUMERO_ALMACEN)
-                     );
-                     
-CREATE TABLE VENDEDOR(NUMERO_VENDEDOR INTEGER, NOMBRE_VENDEDOR VARCHAR2(60),
-                      AREA_VENTAS VARCHAR2(40),
-                      CONSTRAINT PK_NV PRIMARY KEY(NUMERO_VENDEDOR)
-                      );
-                        
-         
-      CREATE TABLE CLIENTE(NUMERO_CLIENTE INTEGER, NUMERO_ALMACEN INTEGER,
-      NOMBRE_CLIENTE VARCHAR2(50),
-      CONSTRAINT PK_NC PRIMARY KEY(NUMERO_CLIENTE),
-      CONSTRAINT FK_NA1 FOREIGN KEY(NUMERO_ALMACEN) REFERENCES ALMACEN(NUMERO_ALMACEN)
-      );
-      
-      CREATE TABLE VENTAS(ID_VENTAS INTEGER, NUMERO_CLIENTE INTEGER, 
-      NUMERO_VENDEDOR INTEGER, MONTO_VENTA FLOAT,
-      CONSTRAINT PK_IDV PRIMARY KEY(ID_VENTAS),
-      CONSTRAINT FK_NC1 FOREIGN KEY(NUMERO_CLIENTE) REFERENCES CLIENTE(NUMERO_CLIENTE),
-      CONSTRAINT FK_NV1 FOREIGN KEY(NUMERO_VENDEDOR) REFERENCES VENDEDOR(NUMERO_VENDEDOR)
-      );
-      
-      CREATE SEQUENCE SEC_VENTAS
-      START WITH 1
-      INCREMENT BY 1
-      NOMAXVALUE;
+CREATE TABLE almacen (
+    numero_almacen      INTEGER,
+    ubicacion_almacen   VARCHAR2(40),
+    CONSTRAINT pk_na PRIMARY KEY ( numero_almacen )
+);
+
+CREATE TABLE vendedor (
+    numero_vendedor   INTEGER,
+    nombre_vendedor   VARCHAR2(60),
+    area_ventas       VARCHAR2(40),
+    CONSTRAINT pk_nv PRIMARY KEY ( numero_vendedor )
+);
+
+CREATE TABLE cliente (
+    numero_cliente   INTEGER,
+    numero_almacen   INTEGER,
+    nombre_cliente   VARCHAR2(50),
+    CONSTRAINT pk_nc PRIMARY KEY ( numero_cliente ),
+    CONSTRAINT fk_na1 FOREIGN KEY ( numero_almacen )
+        REFERENCES almacen ( numero_almacen )
+);
+
+CREATE TABLE ventas (
+    id_ventas         INTEGER,
+    numero_cliente    INTEGER,
+    numero_vendedor   INTEGER,
+    monto_venta       FLOAT,
+    CONSTRAINT pk_idv PRIMARY KEY ( id_ventas ),
+    CONSTRAINT fk_nc1 FOREIGN KEY ( numero_cliente )
+        REFERENCES cliente ( numero_cliente ),
+    CONSTRAINT fk_nv1 FOREIGN KEY ( numero_vendedor )
+        REFERENCES vendedor ( numero_vendedor )
+);
+
+CREATE SEQUENCE sec_ventas START WITH 1 INCREMENT BY 1 NOMAXVALUE;
       
       
       --PROCEDIMIENTO ALMACEN 
-      CREATE OR REPLACE PROCEDURE GUARDAR_ALMACEN(MY_NUMERO_ALMACEN IN INTEGER, MY_UBICACION_ALMACEN IN VARCHAR2)
-      AS
-      BEGIN
-      INSERT INTO ALMACEN VALUES(MY_NUMERO_ALMACEN, MY_UBICACION_ALMACEN);
-      END;
-      /
+
+CREATE OR REPLACE PROCEDURE guardar_almacen (
+    my_numero_almacen      IN INTEGER,
+    my_ubicacion_almacen   IN VARCHAR2
+)
+    AS
+BEGIN
+    INSERT INTO almacen VALUES (
+        my_numero_almacen,
+        my_ubicacion_almacen
+    );
+
+END;
+/
       
       --PROCEDIMIENTO VENDEDOR
-      CREATE OR REPLACE PROCEDURE GUARDAR_VENDEDOR(MY_NUMERO_VENDEDOR IN INTEGER, MY_NOMBRE_VENDEDOR IN VARCHAR2,MY_AREA_VENTAS IN VARCHAR2)
-      AS
-      BEGIN
-      INSERT INTO VENDEDOR VALUES(MY_NUMERO_VENDEDOR, MY_NOMBRE_VENDEDOR, MY_AREA_VENTAS);
-      END;
-      /
+
+CREATE OR REPLACE PROCEDURE guardar_vendedor (
+    my_numero_vendedor   IN INTEGER,
+    my_nombre_vendedor   IN VARCHAR2,
+    my_area_ventas       IN VARCHAR2
+)
+    AS
+BEGIN
+    INSERT INTO vendedor VALUES (
+        my_numero_vendedor,
+        my_nombre_vendedor,
+        my_area_ventas
+    );
+
+END;
+/
       
       --PROCEDIMIENTO CLIENTE
-      CREATE OR REPLACE PROCEDURE GUARDAR_CLIENTE(MY_NUMERO_CLIENTE IN INTEGER,MY_NUMERO_ALMACEN IN INTEGER, MY_NOMBRE_CLIENTE IN VARCHAR2)
-      AS
-      BEGIN
-      INSERT INTO CLIENTE VALUES(MY_NUMERO_CLIENTE, MY_NUMERO_ALMACEN, MY_NOMBRE_CLIENTE);
-      END;
-      /
+
+CREATE OR REPLACE PROCEDURE guardar_cliente (
+    my_numero_cliente   IN INTEGER,
+    my_numero_almacen   IN INTEGER,
+    my_nombre_cliente   IN VARCHAR2
+)
+    AS
+BEGIN
+    INSERT INTO cliente VALUES (
+        my_numero_cliente,
+        my_numero_almacen,
+        my_nombre_cliente
+    );
+
+END;
+/
       
       --PROCEDIMIENTO VENTAS
-      
-      CREATE OR REPLACE PROCEDURE GUARDAR_VENTAS(MY_ID OUT INTEGER, MY_NUMERO_CLIENTE IN INTEGER , MY_NUMERO_VENDEDOR IN INTEGER, MY_MONTO_VENTA IN FLOAT)
-      AS BEGIN
-      SELECT SEC_VENTAS.NEXTVAL INTO MY_ID FROM DUAL;
-      INSERT INTO VENTAS VALUES(MY_ID, MY_NUMERO_CLIENTE, MY_NUMERO_VENDEDOR, MY_MONTO_VENTA);
-      END;
-      /
+
+CREATE OR REPLACE PROCEDURE guardar_ventas (
+    my_id                OUT INTEGER,
+    my_numero_cliente    IN INTEGER,
+    my_numero_vendedor   IN INTEGER,
+    my_monto_venta       IN FLOAT
+)
+    AS
+BEGIN
+    SELECT
+        sec_ventas.NEXTVAL
+    INTO
+        my_id
+    FROM
+        dual;
+
+    INSERT INTO ventas VALUES (
+        my_id,
+        my_numero_cliente,
+        my_numero_vendedor,
+        my_monto_venta
+    );
+
+END;
+/
       
       --GUARDARMOS PRIMERO LA MÁS INDEPENDIENTE ALMACEN
-      
-      
-      BEGIN
-      GUARDAR_ALMACEN(1,'PLYMOUTH');
-      GUARDAR_ALMACEN(2,'SUPERIOR');
-      GUARDAR_ALMACEN(3,'BISMARCK');
-      GUARDAR_ALMACEN(4,'FARGO');
-      END;
-      /
-      SELECT * FROM ALMACEN;
+
+BEGIN
+    guardar_almacen(
+        1,
+        'PLYMOUTH'
+    );
+    guardar_almacen(
+        2,
+        'SUPERIOR'
+    );
+    guardar_almacen(
+        3,
+        'BISMARCK'
+    );
+    guardar_almacen(
+        4,
+        'FARGO'
+    );
+END;
+/
+
+SELECT
+    *
+FROM
+    almacen;
       
       --LA SIGUIENTE INDEPENDIENTE  A GUARDAR ES CLIENTE
-      
-      BEGIN
-      GUARDAR_CLIENTE(18765,4,'DELTA SYSTEMS');
-    GUARDAR_CLIENTE(18830,3,'A. LEVY AND SONS');
-    GUARDAR_CLIENTE(19242,3,'RANIER COMPANY');
-    GUARDAR_CLIENTE(18841,2,'R.W. FLOOD INC.');
-    GUARDAR_CLIENTE(18899,2,'SEWARD SYSTEMS');
-    GUARDAR_CLIENTE(19565,1,'STODOLA''S INC.');
-    END;
-    /
-    
-    SELECT * FROM CLIENTE;
+
+BEGIN
+    guardar_cliente(
+        18765,
+        4,
+        'DELTA SYSTEMS'
+    );
+    guardar_cliente(
+        18830,
+        3,
+        'A. LEVY AND SONS'
+    );
+    guardar_cliente(
+        19242,
+        3,
+        'RANIER COMPANY'
+    );
+    guardar_cliente(
+        18841,
+        2,
+        'R.W. FLOOD INC.'
+    );
+    guardar_cliente(
+        18899,
+        2,
+        'SEWARD SYSTEMS'
+    );
+    guardar_cliente(
+        19565,
+        1,
+        'STODOLA''S INC.'
+    );
+END;
+/
+
+SELECT
+    *
+FROM
+    cliente;
       
     --SIGUIENTE INDEPENDIENTE VENDEDOR  
-    
-    BEGIN
-    GUARDAR_VENDEDOR(3462,'WATERS','WEST');
-    GUARDAR_VENDEDOR(3593,'DRYNE','EAST');
-    END;
-    /
-    
-    SELECT * FROM VENDEDOR;
+
+BEGIN
+    guardar_vendedor(
+        3462,
+        'WATERS',
+        'WEST'
+    );
+    guardar_vendedor(
+        3593,
+        'DRYNE',
+        'EAST'
+    );
+END;
+/
+
+SELECT
+    *
+FROM
+    vendedor;
     
     --ULTIMA DEPENDIENTE VENTAS
-    DECLARE
-    VALOR INTEGER;
-    BEGIN
-    GUARDAR_VENTAS(VALOR, 18765,3462,13540);
-    GUARDAR_VENTAS(VALOR, 18830,3462,10600);
-    GUARDAR_VENTAS(VALOR, 19242,3462,9700);
-    GUARDAR_VENTAS(VALOR, 18841,3593,11560);
-    GUARDAR_VENTAS(VALOR, 18899,3593,2590);
-    GUARDAR_VENTAS(VALOR, 19565,3593,8800);
-    END;
-    /
-    SELECT * FROM VENTAS;
+
+DECLARE
+    valor   INTEGER;
+BEGIN
+    guardar_ventas(
+        valor,
+        18765,
+        3462,
+        13540
+    );
+    guardar_ventas(
+        valor,
+        18830,
+        3462,
+        10600
+    );
+    guardar_ventas(
+        valor,
+        19242,
+        3462,
+        9700
+    );
+    guardar_ventas(
+        valor,
+        18841,
+        3593,
+        11560
+    );
+    guardar_ventas(
+        valor,
+        18899,
+        3593,
+        2590
+    );
+    guardar_ventas(
+        valor,
+        19565,
+        3593,
+        8800
+    );
+END;
+/
+
+SELECT
+    *
+FROM
+    ventas;
     
     
       --quiero saber el nombre del vendendor el monto de ventas
-      
-      declare
-      cursor cur_ventas is select * from ventas;
-      cursor cur_vendedor is select * from vendedor;
-      begin
-      for rec_ventas in cur_ventas loop
-        for rec_vendedor in cur_vendedor loop
-        if rec_vendedor.numero_vendedor=rec_ventas.numero_vendedor then
-        dbms_output.put_line(''||rec_ventas.id_ventas ||'  '||rec_vendedor.nombre_vendedor ||'  '||rec_ventas.monto_venta);
-        end if;
-        end loop;
-        end loop;
-        end;
-        /
-        set serveroutput on;
-      
-      
-      
-         
-         
-         
-         
-         
-         
-                      
-                      
+
+DECLARE
+    CURSOR cur_ventas IS
+        SELECT
+            *
+        FROM
+            ventas;
+
+    CURSOR cur_vendedor IS
+        SELECT
+            *
+        FROM
+            vendedor;
+
+BEGIN
+    FOR rec_ventas IN cur_ventas LOOP
+        FOR rec_vendedor IN cur_vendedor LOOP
+            IF
+                rec_vendedor.numero_vendedor = rec_ventas.numero_vendedor
+            THEN
+                dbms_output.put_line(''
+                 || rec_ventas.id_ventas
+                 || '  '
+                 || rec_vendedor.nombre_vendedor
+                 || '  '
+                 || rec_ventas.monto_venta);
+
+            END IF;
+        END LOOP;
+    END LOOP;
+END;
+/
+
+SET SERVEROUTPUT ON;
+
+CREATE TABLE direccion (
+    id          INTEGER PRIMARY KEY,
+    numero      INTEGER,
+    calle       VARCHAR2(60),
+    cp          INTEGER,
+    municipio   VARCHAR2(80)
+);
+
+CREATE SEQUENCE sec_direccion START WITH 1 INCREMENT BY 1 NOMAXVALUE;
+
+CREATE OR REPLACE TRIGGER disp_direccion BEFORE
+    INSERT ON direccion
+    FOR EACH ROW
+BEGIN
+    SELECT
+        sec_direccion.NEXTVAL
+    INTO
+        :new.id
+    FROM
+        dual;
+
+END;
+/
+
+SELECT   * FROM  direccion;
+
+select * from direccion;
+insert into direccion values(1,12,'13',12,'ecatepec');
